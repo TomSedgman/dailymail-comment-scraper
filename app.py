8074d3c28c6bda9f5d25511d2c041a87daaa4c8d
Aaron <aaron.ford90@gmail.com>
2015-12-01 20:32:31 +0000

    First attempt on functionality to get comment and return it as json

--- app.py
+++ app.py
@@ -1,125 +1,136 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-try:
-    # For Python 3.0 and later
-    from urllib.request import urlopen
-    from urllib import HTTPError, URLError
-except ImportError:
-    # Fall back to Python 2's urllib2
-    from urllib2 import urlopen, HTTPError, URLError
-import json, base64, re
-import xmltodict
-from random import randint
+import os
+from os import environ as env
+from sys import argv
 
-# Function to get json data from a url 
-def get_jsonparsed_data(url):
+import bottle
+from bottle import default_app, request, route, response, get
 
+bottle.debug(True)
+
+@get('/')
+def index():
+  response.content_type = 'text/json; charset=utf-8'
+  try:
+      # For Python 3.0 and later
+      from urllib.request import urlopen
+      from urllib import HTTPError, URLError
+  except ImportError:
+      # Fall back to Python 2's urllib2
+      from urllib2 import urlopen, HTTPError, URLError
+  import base64, re, lxml, xmltodict
+  from lxml import objectify
+  import simplejson as json
+  from random import randint
+
+  # Function to get json data from a url 
+  def get_jsonparsed_data(url):
+
+      try:
+         response = urlopen(url)
+      except HTTPError, err:
+         if err.code == 404:
+             return "Page not found!"
+         elif err.code == 403:
+             return "Access denied!"
+         else:
+             return "Something happened! Error code", err.code
+      except URLError, err:
+          return "Some other error happened:", err.reason
+
+      data = str(response.read())
+      return json.loads(data);
+
+  # Function to get xml data as json from a url: 
+  def get_xml(request):
+      file = urlopen(request)
+      data = file.read()
+      file.close()
+
+      xmlData = xmltodict.parse(data)
+      return xmlData
+
+  # URL for Daily Mail's RSS feed:
+  urlArticleList = "http://www.dailymail.co.uk/home/index.rss"
+
+  # How many times to retry
+  done = 0
+  maxTries = 50000
+
+  while done < maxTries:
+
+    # Get all stories from the rss feed
     try:
-       response = urlopen(url)
-    except HTTPError, err:
-       if err.code == 404:
-           return "Page not found!"
-       elif err.code == 403:
-           return "Access denied!"
-       else:
-           return "Something happened! Error code", err.code
-    except URLError, err:
-        return "Some other error happened:", err.reason
 
-    data = str(response.read())
-    return json.loads(data);
+      xmlDataStories = get_xml(urlArticleList)
+      jsonDataStories = json.dumps(xmlDataStories)
+      jsonDataStoriesLoaded = json.loads(jsonDataStories)
 
-# Function to get xml data as json from a url: 
-
-def get_xml_as_json(request):
-    file = urlopen(request)
-    data = file.read()
-    file.close()
-
-    data = xmltodict.parse(data)
-    return json.dumps(data)
+    except:
+      done +=1
+      continue
 
 
-urlArticleList = "http://www.dailymail.co.uk/home/index.rss"
-# done = 0
-# maxTries = 10
+    # Pick random story
+    try:
+      storiesNumber = len(jsonDataStoriesLoaded["rss"]["channel"]["item"])
+      storyNumber = randint(0,(storiesNumber - 1))
 
-# while done < maxTries:
+    except:
+      done +=1
+      continue
 
-# Get the 'hottest' stories for comments
-jsonDataStories = get_xml_as_json(urlArticleList)
+    # Get the Stpry ID and clean up the URL
+    try:  
+      storyURL = jsonDataStoriesLoaded["rss"]["channel"]["item"][storyNumber]["link"]
+      shortStoryURL = storyURL.split('?', 1)[0]
+      storyIDAlmost = shortStoryURL.split('-', 1)[-1] 
+      storyID = storyIDAlmost.split('/', 1)[0]
 
-#print jsonDataStories 
+    except:
+      done +=1
+      continue
 
-#Pick random story
-# try:
-storiesNumber = len(jsonDataStories["rss"]["channel"]["item"])
-#         storiesNorm = randint(0,(storiesNumber - 1))
-print storiesNumber
+    # Maximum Number of comments we want to get from the API
+    maxCommentNumber = str(50)
 
-#     except:
-#         done +=1
-#         continue
+    # Build the URL to return the 'Best Rated' comments for the random story
+    urlForComments = "http://www.dailymail.co.uk/reader-comments/p/asset/readcomments/"+storyID+"?max="+maxCommentNumber+"&sort=voteRating&order=desc&rcCache=shout"
 
-#     try:
+    # Get comment and metadata
+    try:
+      jsonDataComments = get_jsonparsed_data(urlForComments)
+      commentsNumber = len(jsonDataComments["payload"]["page"])
+      randomCommentNumber = randint(0,(commentsNumber - 1))
+      commentBody = jsonDataComments["payload"]["page"][randomCommentNumber]["message"]
+      userName = jsonDataComments["payload"]["page"][randomCommentNumber]["userAlias"]
+      downVotes = int((jsonDataComments["payload"]["page"][randomCommentNumber]["voteCount"] - jsonDataComments["payload"]["page"][randomCommentNumber]["voteRating"]) * 0.5)
+      upVotes = int(jsonDataComments["payload"]["page"][randomCommentNumber]["voteRating"] + downVotes)
 
-#         # Read the 'Hottest' story title and ID
-#         articleTitle = jsonDataStories["data"][storiesNorm]["title"]
-#         articleID =  str(jsonDataStories["data"][storiesNorm]["articleId"])
-#         # Encode ID using Base 64
-#         articleIDBase64 = base64.b64encode(articleID)
+    except:
+      done +=1
+      continue
 
-#     except:
-#         done +=1
-#         continue
+    # Strip out html tags
+    def cleanhtml(raw_html):
 
-#     # Build URL for the top comments on the article 
-#     urlTopLikes = "http://bskyb.bootstrap.fyre.co/api/v3.0/site/360818/article/"+articleIDBase64+"/top/likes/"
+      cleanr =re.compile('<.*?>')
+      cleantext = re.sub(cleanr,'', raw_html)
+      return cleantext;
 
-#     # Get top comments
-#     try: 
-#         jsonDataComments = get_jsonparsed_data(urlTopLikes)
-        
-#     except: 
-#         done +=1
-#         continue
+    filth = cleanhtml(commentBody)+" - "+userName
 
-#     # Pick a random comment
+    break
 
-#     try:
-#         ratedComments = len(jsonDataComments["data"]["content"])
-#         ratedCommentNorm = randint(0,(ratedComments - 1))
-#     except: 
-#         done +=1
-#         continue
+  if done == maxTries:
+    errorString = "Sorry, They're busy killing kittens"
+    return {"comment": errorString}
     
-#     #Read Horrible Comment and number of likes
-#     try: 
+  else:
+  # Return the horrible comment
+    return {"comment": filth , "storyTitle": shortStoryURL, "numberOfLikes": upVotes, "numberOfDislikes": downVotes}
 
-#         horribleCommentHTML = jsonDataComments["data"]["content"][ratedCommentNorm]["content"]["bodyHtml"]
-#         numberOfLikes = jsonDataComments["data"]["content"][ratedCommentNorm]["content"]["annotations"]["likes"]
-    
-#     except: 
-#         done +=1
-#         continue
+bottle.run(host='0.0.0.0', port=argv[1])
 
-#     #Strip out html tags
-#     def cleanhtml(raw_html):
-
-#         cleanr =re.compile('<.*?>')
-#         cleantext = re.sub(cleanr,'', raw_html)
-#         return cleantext;
-
-#     filth = cleanhtml(horribleCommentHTML)
-
-#     break
-
-# if done == maxTries:
-#     errorString = "Sorry, LiveFyre's API is being about as good as Vigiglobe's"
-#     return {"comment": errorString}
-
-
-# else:
-#     # Return the horrible comment
-#     return {"comment": filth, "storyTitle": articleTitle, "numberOfLikes": numberOfLikes}
