2d8957b419a4035bfa5f9b1ed75df1e49a232761
Tom Sedgman <t.sedgman@gmail.com>
2022-08-16 15:29:17 +0100

    deploy1

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ def index():
         return xmlData
 
     # URL for Daily Mail's RSS feed:
-    data_source = request.get("article")
+    data_source = request.query_string
     urlArticleList = "http://www.dailymail.co.uk/home/index.rss"
 
     # How many times to retry
